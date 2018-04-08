9afd37790bd7c80ae9e68421c9ae61f3a69fa7d8
Daniel Furman <djfurman@gmail.com>
2018-04-08 16:16:15 -0400

    Refactor unit test file to use pytest

--- compute/example/tests/test_uuid_responder.py
+++ compute/example/tests/test_uuid_responder.py
@@ -1,11 +1,6 @@
-import unittest
 from example.example import get_uuid
 
 
-class TddUuidExample(unittest.TestCase):
-    def test_it_returns_a_uuid_v4(self):
-        result = get_uuid()
-        self.assertEqual(36, len(result))
-
-if __name__ == '__main__':
-    unittest.main()
+def test_it_returns_a_uuid_v4():
+    result = get_uuid()
+    assert len(result) == 36
