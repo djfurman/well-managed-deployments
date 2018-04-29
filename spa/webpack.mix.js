8367b3665f157d9775f20d0fde3880cf2e6d98f2
Daniel Furman <djfurman@gmail.com>
2018-04-29 09:06:49 -0400

    Update build steps

--- spa/webpack.mix.js
+++ spa/webpack.mix.js
@@ -3,6 +3,7 @@ let mix = require('laravel-mix');
 mix.js('src/app.js', 'dist')
     .sass('src/app.scss', 'dist')
     .extract([
+        'aws-amplify',
         'moment',
         'sweetalert2',
         'vue',
